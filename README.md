4821b89d53998d028ddff0788e2eaec365961365
xilin693 <81901999@qq.com>
2022-04-07 16:57:36 +0800

    添加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kapi
  kapi后端
+* 使用需修改.env文件,按实际的数据库及redis修改
+* kapi插件在tools目录下,需要解压后,在chrome开发者工具中加载该插件
+* kapi数据库在tools目录下,使用需要导入到数据库中运行
+
---
+++ application/controller/www/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controller\www;
+
+use king\lib\permission\LoginHelper;
+
+class Test
+{
+    public function index()
+    {
+        echo "kapi后端运行中";
+    }
+}
