6d7745f25c092a118369e62be1b84b9767dc0186
xilin693 <81901999@qq.com>
2022-04-10 17:51:51 +0800

    Update sentry.php

--- application/config/sentry.php
+++ application/config/sentry.php
@@ -3,5 +3,5 @@
 use king\lib\Env;
 
 return [
-    'dsn' => Env::get('sentry.dsn', 'http://909d660429374a368a30285dc53c224c:2656185669ee46a5863c58d8a6c6141b@114.116.167.2:9201/3'),
+    'dsn' => Env::get('sentry.dsn', 'http://xxx'),
 ];
